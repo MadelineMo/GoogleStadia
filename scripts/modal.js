9b2d42b454c018b5f99d395a324f4859847f787d
MadelineMo <mmoran11@luc.edu>
2024-04-19 01:12:53 -0500

    modal works

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,8 +1,8 @@
-// Get the modal https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal
-var modal = document.getElementById("gowModal");
+// Get the modal
+var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("modalBtn");
+var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -20,6 +20,6 @@ span.onclick = function() {
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+      modal.style.display = "none";
   }
-}
+} 
